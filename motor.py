dafc1f08cc0d49c79a7ea5b50c9c3a29b6b900c2
saysaeqo <saysaeqo@gmail.com>
2025-07-20 12:32:40 +0200

    adjustment

--- motor.py
+++ motor.py
@@ -2,16 +2,16 @@ import RPi.GPIO as GPIO
 from time import sleep, perf_counter
 
 FULL_ROTATION = 200
-
+NOT_ASSIGNED = 26
 PINS = {
-    "M1": 14,
-    "M2": 15,
-    "M3": 18,
-    "DIR": 3,
-    "STEP": 2,
-    "EN": 17,
-    "SLP": 27,
-    "RST": 4
+    "M1": NOT_ASSIGNED,
+    "M2": NOT_ASSIGNED,
+    "M3": NOT_ASSIGNED,
+    "DIR": 27,
+    "STEP": 17,
+    "EN": NOT_ASSIGNED,
+    "SLP": NOT_ASSIGNED,
+    "RST": NOT_ASSIGNED
 }
 
 def setup():
@@ -27,16 +27,17 @@ def reset():
 def generate_sine_wave(frequency=1, duration=1):
     """Generate a sine wave for the given frequency and duration."""
     start_time = perf_counter()
+    wait_time = 1 / (2 * frequency)
     while perf_counter() - start_time < duration:
         yield GPIO.HIGH
-        sleep(1 / (2 * frequency))
+        sleep(wait_time)
         yield GPIO.LOW
-        sleep(1 / (2 * frequency))
+        sleep(wait_time)
 
 def step(count, direction=GPIO.LOW):
     """Rotate count steps. direction = 1 means backwards"""
     GPIO.output(PINS["DIR"], direction)
-    for state, _ in zip(generate_sine_wave(200), range(200)):
+    for state, _ in zip(generate_sine_wave(100,2), range(200)):
         GPIO.output(PINS["STEP"], state)
 
 if __name__ == "__main__":
