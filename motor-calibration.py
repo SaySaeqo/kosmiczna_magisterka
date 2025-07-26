cbb62c26481c19a3a04473a135206bd2770f3f63
saysaeqo <saysaeqo@gmail.com>
2025-07-26 20:15:04 +0200

    Fix: range can only be ints

--- motor-calibration.py
+++ motor-calibration.py
@@ -17,15 +17,17 @@ if __name__ == "__main__":
                 unit = u - 1
                 break
         
-        for t in range(0.1, 1, 0.1):
+        for t in range(1, 10):
+            t = t / 10.0
             motor.INERTIA_PLATFORM2WHEEL_RATIO = unit + t
             motor.rotate_platform(math.pi, 1, 100)
             next_step = input(f"Is {unit + t} to much? (y/n): ").strip().lower()
             if next_step == "y":
                 tenths = t - 0.1
                 break
-        
-        for h in range(0.01, 0.1, 0.01):
+
+        for h in range(1, 10):
+            h = h / 100.0
             motor.INERTIA_PLATFORM2WHEEL_RATIO = unit + tenths + h
             motor.rotate_platform(math.pi, 1, 100)
             next_step = input(f"Is {unit + tenths + h} to much? (y/n): ").strip().lower()
