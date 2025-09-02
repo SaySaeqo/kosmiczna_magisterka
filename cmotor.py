60d1db5a5bc94c9c7036e950dcd9f70c6b337b0b
saysaeqo <saysaeqo@gmail.com>
2025-09-02 14:45:49 +0200

    Added: adhoc crotacc method

--- cmotor.py
+++ cmotor.py
@@ -2,12 +2,6 @@ import kosmiczna_magisterka.fast_motor as fast_motor
 from time import perf_counter
 import motor
 
-def c_test_signal(pin, how_much):
-    start = perf_counter()
-    fast_motor.test_signal(pin, how_much)
-    end = perf_counter()
-    print(f"Generated {how_much / (end - start):.6f} impulses per second")
-
 def c_generate_signal(pin: int, acc: float, freq: int, duration: float):
     acc_const = acc / motor.ROTATION_PER_STEP / motor.get_step_resolution()
     fast_motor.generate_signal(pin, acc_const, freq, duration)
@@ -15,3 +9,7 @@ def c_generate_signal(pin: int, acc: float, freq: int, duration: float):
 def c_generate_signal2(pin: int, acc: float, freq: int, duration: float):
     acc_const = acc / motor.ROTATION_PER_STEP / motor.get_step_resolution()
     fast_motor.generate_signal2(pin, acc_const, freq, duration)
+
+def c_generate_signal3(pin: int, acc: float, freq: int, duration: float):
+    acc_const = acc / motor.ROTATION_PER_STEP / motor.get_step_resolution()
+    fast_motor.generate_signal3(pin, acc_const, freq, duration)
--- fast_motor.c
+++ fast_motor.c
@@ -5,28 +5,19 @@
 #define ASSERT_SUCCESS(status, msg) \
     if (status < 0) { \
         PyErr_SetString(PyExc_Exception, msg); \
-        return NULL; \
-    }
-
-static PyObject* test_signal(PyObject* self, PyObject* args)
-{
-    int pin;
-    int how_much;
-    if (!PyArg_ParseTuple(args, "ii", &pin, &how_much))
-    {
-        return NULL;
+        return -1; \
     }
     
-    ASSERT_SUCCESS(gpioInitialise(), "Failed to initialize GPIO");
-    ASSERT_SUCCESS(gpioSetMode(pin, PI_OUTPUT), "Failed to set GPIO mode");
-    for (int i = 0; i < how_much; i++)
-    {
-        ASSERT_SUCCESS(gpioWrite(pin, 1), "Failed to write GPIO");
-        ASSERT_SUCCESS(gpioWrite(pin, 0), "Failed to write GPIO");
-    }
+static void fast_motor_atexit(void) {
     gpioTerminate();
+}
 
-    Py_RETURN_NONE;
+static int
+fast_motor_module_exec(PyObject *m)
+{
+    ASSERT_SUCCESS(gpioInitialise(), "Failed to initialize PIGPIO");
+    ASSERT_SUCCESS(Py_AtExit(fast_motor_atexit), "Failed to register PIGPIO exit handler");
+    return 0;
 }
 
 static PyObject* generate_signal(PyObject* self, PyObject* args)
@@ -72,9 +63,6 @@ static PyObject* generate_signal(PyObject* self, PyObject* args)
         nwait_times[nwait_times_length++] = sleep_time;
     }
 
-    ASSERT_SUCCESS(gpioInitialise(), "Failed to initialize GPIO");
-    ASSERT_SUCCESS(gpioSetMode(pin, PI_OUTPUT), "Failed to set GPIO mode");
-
     int start_loop, end_loop;
 
     Py_BEGIN_ALLOW_THREADS
@@ -101,8 +89,6 @@ static PyObject* generate_signal(PyObject* self, PyObject* args)
         // printf("Impulse duration: %d microseconds\n", sleep_time * 2);
     }
     Py_END_ALLOW_THREADS
-    
-    gpioTerminate();
 
     printf("Max freq: %f\tLast freq: %f\n", 1.0/wait_times[wait_times_length-1]*500000.0, 1.0/nwait_times[nwait_times_length-1]*500000.0);
     Py_RETURN_NONE;
@@ -110,6 +96,7 @@ static PyObject* generate_signal(PyObject* self, PyObject* args)
 
 static PyObject* generate_signal2(PyObject* self, PyObject* args)
 {
+    int calculations_start = gpioTick();
     int pin;
     float acc_const, duration;
     int freq;
@@ -133,38 +120,66 @@ static PyObject* generate_signal2(PyObject* self, PyObject* args)
         wait_times[wait_times_length++] = sleep_time;
     }
 
-    ASSERT_SUCCESS(gpioInitialise(), "Failed to initialize GPIO");
-    ASSERT_SUCCESS(gpioSetMode(pin, PI_OUTPUT), "Failed to set GPIO mode");
-
-    int start_loop, end_loop;
+    int calculations_time = gpioTick() - calculations_start;
 
     Py_BEGIN_ALLOW_THREADS
     for (int i = 0; i < wait_times_length; i++)
     {
         // start_loop = gpioTick();
         gpioWrite(pin, 1);
-        gpioSleep(PI_TIME_RELATIVE, 0, wait_times[i]);
+        gpioDelay(wait_times[i]);
         gpioWrite(pin, 0);
-        gpioSleep(PI_TIME_RELATIVE, 0, wait_times[i]);
+        gpioDelay(wait_times[i]);
         // end_loop = gpioTick();
         // printf("Loop duration: %d microseconds\n", end_loop - start_loop);
         // printf("Impulse duration: %d microseconds\n", sleep_time * 2);
     }
     Py_END_ALLOW_THREADS
-    
-    gpioTerminate();
 
     printf("Max freq: %f\tLast wait time: %f\n", 1.0/wait_times[wait_times_length-1]*500000.0, wait_times[wait_times_length-1]);
     Py_RETURN_NONE;
 }
 
+static PyObject* generate_signal3(PyObject* self, PyObject* args)
+{
+    int init_start = gpioTick();
+    int pin;
+    float acc_const, duration;
+    int freq;
+    if (!PyArg_ParseTuple(args, "ifif", &pin, &acc_const, &freq, &duration))
+    {
+        return NULL;
+    }
+
+    Py_BEGIN_ALLOW_THREADS
+
+    float time_passed = 0.0;
+    float impulse_duration = 1.0 / freq;
+
+    int calculations_start = gpioTick();
+    useconds_t sleep_time = (useconds_t)(impulse_duration * 500000);
+    time_passed += impulse_duration;
+    impulse_duration = 1.0 / (freq + acc_const * time_passed);
+    useconds_t calculations_time = (useconds_t)(gpioTick() - calculations_start);
+
+    while (time_passed < duration + impulse_duration)
+    {
+        gpioWrite(pin, 1);
+        gpioDelay(sleep_time - calculations_time);
+        gpioWrite(pin, 0);
+        gpioDelay(sleep_time);
+        sleep_time = (useconds_t)(impulse_duration * 500000);
+        time_passed += impulse_duration;
+        impulse_duration = 1.0 / (freq + acc_const * time_passed);
+    }
+    Py_END_ALLOW_THREADS
+
+    printf("Calculations time: %d us\tInit time: %d us\tLast impulse duration: %d us\n", calculations_time, calculations_start - init_start, sleep_time*2);
+
+    Py_RETURN_NONE;
+}
+
 static PyMethodDef fast_motor_funcs[] = {
-	{	
-        "test_signal",
-		test_signal,
-		METH_VARARGS,
-		"Generates a wave signal for given parameters."
-    },
     {	
         "generate_signal",
 		generate_signal,
@@ -177,7 +192,18 @@ static PyMethodDef fast_motor_funcs[] = {
 		METH_VARARGS,
 		"Generates a wave signal for given parameters. (simple version)"
     },
+    {	
+        "generate_signal3",
+		generate_signal3,
+		METH_VARARGS,
+		"Generates a wave signal for given parameters. (adhoc calc version)"
+    },
 	{NULL, NULL, 0, NULL} // Sentinel
+};
+
+static PyModuleDef_Slot fast_motor_module_slots[] = {
+    {Py_mod_exec, fast_motor_module_exec},
+    {0, NULL}
 };
 
 static struct PyModuleDef fast_motor_module = {
@@ -186,8 +212,9 @@ static struct PyModuleDef fast_motor_module = {
     .m_size = 0, // allow module re-initialization
     .m_doc = "Module for accelerating stepper motor wave generations.",
     .m_methods = fast_motor_funcs,
+    .m_slots = fast_motor_module_slots,
 };
 
 PyMODINIT_FUNC PyInit_fast_motor(void) {
-	return PyModule_Create(&fast_motor_module);
+    return PyModuleDef_Init(&fast_motor_module);
 }
